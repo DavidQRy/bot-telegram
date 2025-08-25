4d979a1f692e296a990df2ce43cdbb8456b67e0b
DavidQRy <davidgui393@gmail.com>
2025-08-24 21:16:25 -0500

    finish bot of Telegram

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 import { Telegraf } from "telegraf";
 import dotenv from "dotenv";
+import fs from 'fs'
+import axios from "axios";
+
+const downloandImage = (url, image_path, ctx) => 
+    axios({url, responseType: 'stream'}).then(
+        (response) => 
+            new Promise((resolve, reject) =>{
+                response.data
+                .pipe(fs.createWriteStream(image_path))
+                .on("finish", () => {
+                    ctx.reply('photo save');
+                    resolve()
+                }).on('error', (e) => {
+                    ctx.reply('The photo could not be saved')
+                    reject(e)
+                })
+            })
+    )
 
 dotenv.config()
+
+function randomNumber(number) {
+ return Math.floor(Math.random() * (number + 1)) 
+}
 
 const bot = new Telegraf(process.env.TOKEN_BOT_TELEGRAM)
 bot.start((ctx) => {
@@ -13,8 +35,30 @@ bot.help((ctx) => {
  ctx.reply("You are in a test bot")
 })
 bot.command('random', (ctx) => {
-    console.log(ctx.message.chat.id)
-    ctx.reply(Math.floor(Math.random() * 101 ))
+    console.log(ctx)
+    ctx.reply(randomNumber(100))
+})
+
+bot.command('advancerandom', (ctx) => {
+    const message = ctx.update.message.text
+    const randoNumber = Number(message.split(' ')[1])
+    if(isNaN(randoNumber) || randoNumber <= 0) {
+     ctx.reply('Please type /advancerandom by a number greater than 0')
+    }else{
+     ctx.reply(randomNumber(randoNumber))
+    }
+
+})
+
+// bot.on('text', (ctx) =>{
+//     ctx.reply(ctx.update.message.text)
+// })
+
+bot.on('photo', (ctx) => {
+    console.log(ctx)
+    const fileId = ctx.update.message.photo[3].file_id
+    ctx.telegram.getFileLink(fileId).then((response) => downloandImage(response, `./uploads/${fileId}.jpg`, ctx))
+    ctx.reply('this is a photo.')
 })
 
 bot.launch() 
